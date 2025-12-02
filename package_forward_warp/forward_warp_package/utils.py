cde66d057708b353e1e5bb499be5639c77cccfc2
Matheus Kunzler Maldaner <matheus@sisnema.com.br>
2025-12-02 12:10:32 -0500

    update code to match new meshzoo expectations

--- package_forward_warp/forward_warp_package/utils.py
+++ package_forward_warp/forward_warp_package/utils.py
@@ -3,13 +3,13 @@ import meshzoo
 
 def generate_2D_mesh(H, W):
     _, faces = meshzoo.rectangle(
-        xmin = -1., xmax = 1.,
-        ymin = -1., ymax = 1.,
-        nx = W, ny = H,
-        zigzag=True)
+        x0=-1., x1=1.,
+        y0=-1., y1=1.,
+        nx=W, ny=H)
 
-    x = torch.arange(0, W, 1).float().cuda() 
-    y = torch.arange(0, H, 1).float().cuda()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    x = torch.arange(0, W, 1).float().to(device) 
+    y = torch.arange(0, H, 1).float().to(device)
 
     xx = x.repeat(H, 1)
     yy = y.view(H, 1).repeat(1, W)
